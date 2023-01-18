594daab15df0f93b4b573b4f2280718ec6d70060
Abito21 <abidjuliant@gmail.com>
2023-01-18 20:43:48 +0700

    check-box

--- app.py
+++ app.py
@@ -13,9 +13,17 @@ def main(page: ft.Page):
     page.bgcolor = "#00337C"
     textField = ft.TextField(width=350)
     addBtn = ft.ElevatedButton(text="Add")
-    entriesRow = ft.Row(controls=[textField, addBtn], alignment=ft.MainAxisAlignment.SPACE_EVENLY)
 
-    page.add(entriesRow)
+    checkBox = ft.Checkbox(value=True)
+    checkBoxText = ft.Text(value="Task 1", width=350, bgcolor="WHITE", size=24)
+
+    taskRow = ft.Row(controls=[checkBox, checkBoxText],
+                     alignment=ft.MainAxisAlignment.START)
+    entriesRow = ft.Row(controls=[textField, addBtn], 
+                        alignment=ft.MainAxisAlignment.SPACE_BETWEEN)
+
+
+    page.add(entriesRow, taskRow)
 
 
 ft.app(target=main)
