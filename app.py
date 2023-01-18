ed022136d681801480dd6d12dca37106347ae030
Abito21 <abidjuliant@gmail.com>
2023-01-18 21:22:09 +0700

    add-task

--- app.py
+++ app.py
@@ -8,22 +8,30 @@ import flet as ft
 
 def main(page: ft.Page):
 
+    def addTask(p):
+        checkBox = ft.Checkbox(value=False)
+        checkBoxText = ft.Text(value=textField.value, width=350, bgcolor="#00337C", size=15, color="WHITE")
+        taskRow = ft.Row(controls=[checkBox, checkBoxText],
+                         alignment=ft.MainAxisAlignment.START)
+        page.add(taskRow)
+
     page.window_width = 500
     page.window_height = 900
     page.bgcolor = "#00337C"
-    textField = ft.TextField(width=350)
-    addBtn = ft.ElevatedButton(text="Add")
 
+    # Text Box
+    textField = ft.TextField(width=350)
+    addBtn = ft.ElevatedButton(text="Add", on_click=addTask)
+
+    # Check Box
     checkBox = ft.Checkbox(value=True)
     checkBoxText = ft.Text(value="Task 1", width=350, bgcolor="WHITE", size=24)
 
-    taskRow = ft.Row(controls=[checkBox, checkBoxText],
-                     alignment=ft.MainAxisAlignment.START)
     entriesRow = ft.Row(controls=[textField, addBtn], 
                         alignment=ft.MainAxisAlignment.SPACE_BETWEEN)
 
 
-    page.add(entriesRow, taskRow)
+    page.add(entriesRow)
 
 
 ft.app(target=main)
